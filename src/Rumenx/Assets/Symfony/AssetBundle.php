04920d317f95903e090e9f1a4ace1d5e883909af
Rumen Damyanov <dev@rumenx.com>
2025-07-29 13:42:55 +0300

    Remove unneeded files

--- src/Rumenx/Assets/Symfony/AssetBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Rumenx\Assets\Symfony;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AssetBundle extends Bundle
-{
-    // Symfony integration for rumenx/php-assets
-}
